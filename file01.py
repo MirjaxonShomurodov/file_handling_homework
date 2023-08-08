e53414afaa4c1ef95a9e72ccdb04a0b0eb8c48f2
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-08 14:33:43 +0500

    1 misol

--- file01.py
+++ file01.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    f=open('data/data01.txt')
+    data=f.read().split(',')
+    for i in range(len(data)):
+        data[i]=int(data[i])
+    return data
+
+print(main('data01.txt'))
+
 
 # Read data from file
+    
+        
+
+
