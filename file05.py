5cb29f78159eec0fba87bd7724501613f12dbdb2
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-08 14:46:26 +0500

    5 misol

--- file05.py
+++ file05.py
@@ -6,5 +6,18 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
-# Read data from file
+    l=[]
+    b=[]
+    n=0
+    a=0
+    for i in range(len(data)):
+        if data[i].isdigit()!=1:
+           n+=1
+        if data[i].isdigit()==1:
+           a+=1
+    return f"[{a},{n}]"
+
+# Read data from file 
+
+f=open('data/data05.txt').read()
+print(main(f))
