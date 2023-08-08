8302583c9c62ccde1971b940c2151576c9bf9dcd
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-08 14:42:29 +0500

    4 misol

--- file04.py
+++ file04.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
-# Read data from file
+    l=[]
+    name=0
+    for i in range(len(data)):
+        if data[i].isalpha():
+           l.append(data[i])
+           i+=1
+    return l  
+
+# Read data from file 
+
+f=open('data/data04.txt').read()
+print(main(f))
