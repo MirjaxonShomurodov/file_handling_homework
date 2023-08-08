df6d9e3714293747fc30aa49895c401f12fa1bbd
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-08 14:38:12 +0500

    3 misol

--- file03.py
+++ file03.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    l=[]
+    name=0
+    for i in range(len(data)):
+        if data[i].isdigit():
+           l.append(data[i])
+           i+=1
+    return l  
 
-# Read data from file
+# Read data from file 
+
+f=open('data/data03.txt').read()
+print(main(f))
