01ccba81f6d48f5967713281e37f8a6b1fc150f3
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-15 05:47:43 +0500

    7 misol

--- file07.py
+++ file07.py
@@ -6,5 +6,23 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    l=[]
+    sum=0
+    for i in range(len(data)):
+        if data[i].isdigit():
+           l.append(data[i])
+        a="".join(l)
+        x=int(a)
+        i=0
+    while x>0:
+           r=x%10
+           r+=x
+           x//=10
+    return r
+
 # Read data from file
+
+f=open('data/data07.txt').read()
+
+print(main(f))
+    
